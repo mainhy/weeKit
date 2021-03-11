c3a6b1d595875c9854e9d39bf06a4c40c04420c3
mainhy <79070181+mainhy@users.noreply.github.com>
2021-03-11 09:13:31 +0700

    Add files via upload

--- weekit.js
+++ weekit.js
@@ -3,30 +3,20 @@
 * Author - BestAddon.com
 * Released under MIT license
 */
-(function (W, D, S) {
+(function (S, window, document, documentElement, next, prev, parent, children, addEventListener, removeEventListener, querySelector, querySelectorAll, getBoundingClientRect, innerHTML, textContent, appendChild) {
   'use strict'
   var _ = {}
   var filter = [].filter
   var map = [].map
-  var forEach = [].forEach
   var push = [].push
   var concat = [].concat
   var slice = [].slice
   var splice = [].splice
   var some = [].some
   var indexOf = [].indexOf
-  var next = 'nextElementSibling' // OR 'nextSibling'
-  var prev = 'previousElementSibling' // OR 'previousSibling'
-  var parent = 'parentElement' // OR 'parentNode'
-  var children = 'children' // OR 'childNodes'
-  var addEvent = 'addEventListener'
-  var s = 'querySelector'
-  var sa = s + 'All'
-  var getBCR = 'getBoundingClientRect'
-  var int = W.parseInt
-  var float = W.parseFloat
-  var div = D.createElement('div')
-  var docEle = D.documentElement
+  var int = window.parseInt
+  var float = window.parseFloat
+  var div = document.createElement('div')
   var checkableRe = /radio|checkbox/i
   var addPx = ['top', 'right', 'bottom', 'left', 'width', 'height', 'minWidth', 'minHeight', 'maxWidth', 'maxHeight', 'fontSize']
 
@@ -34,8 +24,8 @@
   function isSelf (x) { return x instanceof Q }
   function isType (el) { return {}.toString.call(el).slice(8, -1).toLowerCase() }
   'Element,Document,String,Number,Boolean,Null,Undefined,Array,Object,Function,Date'.split(',').forEach(function (type, id) { // Type of Object
-    Q['is' + type] = _[type.slice(0, 3)] = function (o) { // Create private VARIABLE by 3 chars, EX: _.Doc = isDocument
-      if (['Element', 'Document'].indexOf(type) > -1) return o && o.nodeType === Math.pow(9, id)
+    Q['is' + type] = _[type.slice(0, 3)] = function (o) { // Create private VARIABLE by 3 chars, EX: _.Doc = _.Doc
+      if (id < 2) return o && o.nodeType === Math.pow(9, id)
       else return isType(o) === type.toLowerCase()
     }
   })
@@ -43,9 +33,9 @@
     return _.Fun(arg) ? arg.call(context, idx, payload) : arg
   }
   function parseHTML (str) {
-    var tmp = D.implementation.createHTMLDocument()
-    tmp.body.innerHTML = str
-    return tmp.body.children
+    var tmp = document.implementation.createHTMLDocument()
+    tmp.body[innerHTML] = str
+    return tmp.body[children]
   }
   function isPlainObject (obj) {
     return _.Obj(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) === Object.prototype
@@ -53,7 +43,7 @@
   function isSelector (el) {
     if (!el) return false
     try {
-      D.createDocumentFragment().querySelector(el)
+      document.createDocumentFragment()[querySelector](el)
     } catch (e) { return false }
     return true
   }
@@ -63,14 +53,14 @@
   }
   function isValid (el, arg, idx, payload) {
     if (typeof arg === 'object') {
-      Object.keys(arg).length > 0 ? map.call(arg, function (e) { div.appendChild(e) }) : div.appendChild(arg)
-      var divInner = div.innerHTML
-      div.textContent = ''
+      Object.keys(arg).length > 0 ? map.call(arg, function (e) { div[appendChild](e) }) : div[appendChild](arg)
+      var divInner = div[innerHTML]
+      div[textContent] = ''
     }
     return _.Str(divInner) ? divInner : funcArg(el, arg, idx, payload)
   }
   function getStyle (el, prop) {
-    return (el.currentStyle ? el.currentStyle : (W.getComputedStyle ? W.getComputedStyle(el, null) : el.style))[prop]
+    return (window.getComputedStyle(el, null) || el.style)[prop]
   }
   function getCompare (item) {
     return !item ? false
@@ -86,8 +76,8 @@
 
   function goStyle (obj, name, value) {
     if (!obj.length) return
-    return _.Und(value) ? getStyle(obj[0], name) : each(obj, function (i, el) {
-      var addSufix = Q.inArray(name, addPx) > -1 ? 'px' : ''
+    return _.Und(value) ? getStyle(obj[0], name) : goEach(obj, function (i, el) {
+      var addSufix = addPx.indexOf(name) > -1 ? 'px' : ''
       el.style[name] = funcArg(el, value, i, getStyle(el, name)) + addSufix
     })
   }
@@ -102,7 +92,7 @@
     if (!prop) { // For Fn.Filter
       applied(obj)
     } else {
-      each(obj, function (i, el) {
+      goEach(obj, function (i, el) {
         if (_.Fun(prop)) {
           applied(el, prop(el))
         } else {
@@ -130,15 +120,16 @@
     return concat.apply([], arr)
   }
 
-  function each (obj, callback) {
+  function goEach (obj, callback) {
+    var i
     if (isPlainObject(obj)) {
-      for (var i in obj) {
-        if (obj.hasOwnProperty(i)) callback.call(obj[i], i, obj[i])
+      for (i in obj) {
+        callback.call(obj[i], i, obj[i])
       }
     } else {
-      forEach.call(obj, function (ele, i, obj) {
-        callback.call(ele, i, ele, obj)
-      })
+      for (i = 0; i < obj.length; i++) {
+        callback.call(obj[i], i, obj[i])
+      }
     }
     return obj
   }
@@ -160,12 +151,12 @@
     return target
   }
   function ready (callback) {
-    return D.readyState !== 'loading' ? callback() : D[addEvent]('DOMContentLoaded', callback)
+    return document.readyState !== 'loading' ? callback() : document[addEventListener]('DOMContentLoaded', callback)
   }
 
   function Init (el, context) { // el = selector, dom element or function
-    var ctx = context ? (isSelf(context) ? context[0] : context.nodeType ? Q(context)[0] : D[s](context)) : D
-    push.apply(this, el && (el.nodeType || el === W) ? [el] : _.Str(el) ? (isSelector(el) ? ctx[sa](el) : parseHTML(el)) : el)
+    context = context ? (isSelf(context) ? context[0] : context.nodeType ? Q(context)[0] : document[querySelector](context)) : document
+    push.apply(this, el && (el.nodeType || el === window) ? [el] : _.Str(el) ? (isSelector(el) ? context[querySelectorAll](el) : parseHTML(el)) : el)
   }
   function Q (el, context) { // el = selector, dom element or function
     return _.Fun(el) ? ready(el) : new Init(el, context)
@@ -181,7 +172,7 @@
       return ready(callback)
     },
     trigger: function (eventName, options) {
-      var event = D.createEvent(_.Und(options) ? 'HTMLEvents' : 'CustomEvent')
+      var event = document.createEvent(_.Und(options) ? 'HTMLEvents' : 'CustomEvent')
       if (_.Und(options)) event.initEvent(eventName, true, false)
       else event.initCustomEvent(eventName, true, true, options)
       this.each(function (i, el) {
@@ -191,7 +182,7 @@
 
     // /////////*  COLLECTION GROUP  *//////////////// //
     each: function (callback) { // callback = function
-      return each(this, callback)
+      return goEach(this, callback)
     },
     map: function (callback) {
       return Q(concat.apply([], map.call(this, function (ele, i) {
@@ -231,7 +222,7 @@
       var self = this
       var group = {}
       if (_.Str(name)) { return goStyle(self, name, value) }
-      each(name, function (prop, val) {
+      goEach(name, function (prop, val) {
         if (_.Obj(name)) goStyle(self, prop, val)
         else group[val] = goStyle(self, val) // An array of one or more CSS properties.
       })
@@ -251,8 +242,8 @@
       Q(selector).replaceWith(this)
       return this
     },
-    clone: function (deep) {
-      return this[0].cloneNode(_.Und(deep) ? true : deep)
+    clone: function () {
+      return this.map(function (i, el) { return el.cloneNode(true) })
     },
     remove: function (selector) { // selector : String
       this.each(function (i, el) {
@@ -265,7 +256,7 @@
     },
     empty: function () {
       return this.each(function (i, el) {
-        el.textContent = ''
+        el[textContent] = ''
       })
     },
     wrap: function (selector) {
@@ -277,7 +268,7 @@
       return _.Und(selector) ? this : this.each(function (i, el) {
         Q(el).append(_.Obj(selector) ? selector[0] : selector)
         while (el.firstChild !== el.lastChild) {
-          el.lastChild.appendChild(el.firstChild)
+          el.lastChild[appendChild](el.firstChild)
         }
       })
     },
@@ -309,7 +300,7 @@
     },
     find: function (selector) {
       return Q(flat(this.map(function (i, el) {
-        return slice.call(isSelector(selector) ? el[sa](selector) : Q(selector).filter(function (i, ele) {
+        return slice.call(isSelector(selector) ? el[querySelectorAll](selector) : Q(selector).filter(function (i, ele) {
           return el.contains(ele)
         }))
       })))
@@ -361,17 +352,17 @@
     },
     offset: function () {
       if (this[0]) return
-      var box = this[0].getBoundingClientRect()
+      var box = this[0][getBoundingClientRect]()
       return {
-        left: box.left + W.pageXOffset - docEle.clientLeft,
-        top: box.top + W.pageYOffset - docEle.clientTop
+        left: box.left + window.pageXOffset - document[documentElement].clientLeft,
+        top: box.top + window.pageYOffset - document[documentElement].clientTop
       }
     },
     offsetParent: function () {
       return this.map(function () {
         var parent = this.offsetParent
         while (parent && getStyle(parent, 'position') === 'static') { parent = parent.offsetParent }
-        return parent || D.body
+        return parent || document.body
       })
     },
 
@@ -380,7 +371,7 @@
       if (_.Und(value)) return this[0] && (this[0].options && this[0].multiple ? filter.call(this[0].options, function (o) { return o.selected }).map(function (v) { return v.value }) : this[0].value)
       return this.each(function (i, el) {
         if (el.multiple && el.options) {
-          each(el.options, function (i, option) {
+          goEach(el.options, function (i, option) {
             option.selected = value.indexOf(option.value) >= 0
           }, true)
         } else if (checkableRe.test(el.type)) el.checked = value.indexOf(el.value) >= 0
@@ -393,9 +384,9 @@
   // //////////////////////// //
   // *  MANIPULATION GROUP  * //
   // /////////////////////// //
-  each({ append: 'beforeend', prepend: 'afterbegin', after: 'afterend', before: 'beforebegin' }, function (type, position) {
+  goEach({ append: 'beforeend', prepend: 'afterbegin', after: 'afterend', before: 'beforebegin' }, function (type, position) {
     Q.fn[type] = function (html) {
-      return _.Und(html) ? this : each(this, function (idx, el) {
+      return _.Und(html) ? this : goEach(this, function (idx, el) {
         el.insertAdjacentHTML(position, isValid(el, html, idx, el[position]))
       })
     }
@@ -405,9 +396,9 @@
       return this
     }
   })
-  each({ text: 'textContent', html: 'innerHTML' }, function (type, position) {
+  goEach({ text: textContent, html: innerHTML }, function (type, position) {
     Q.fn[type] = function (value) {
-      return _.Und(value) ? this[0][position] : each(this, function (idx, el) {
+      return _.Und(value) ? this[0][position] : goEach(this, function (idx, el) {
         el[position] = isValid(el, value, idx, el[position])
       })
     }
@@ -416,7 +407,7 @@
   // //////////////////////// //
   // *  ATTRIBUTES GROUP  * //
   // /////////////////////// //
-  each(['has', 'add', 'remove', 'toggle'], function (i, type) {
+  goEach(['has', 'add', 'remove', 'toggle'], function (i, type) {
     Q.fn[type + 'Class'] = function (className) {
       if (type === 'has' && _.Str(className)) return this[0].classList.contains(className)
       return this.each(function (idx, el) {
@@ -424,13 +415,13 @@
       })
     }
   })
-  each({ attr: ['get', 'set'], removeAttr: 'remove', hasAttr: ['has'], data: ['get', 'set'] }, function (type, prop) {
+  goEach({ attr: ['get', 'set'], removeAttr: 'remove', hasAttr: ['has'], data: ['get', 'set'] }, function (type, prop) {
     var dataAttr = (type === 'data' ? 'data-' : '')
     Q.fn[type] = function (name, value) {
       if (_.Str(name) && _.Und(value) && _.Arr(prop)) return this[0][prop[0] + 'Attribute'](dataAttr + name)
       return this.each(function (i, el) {
         var isProp = (_.Arr(prop) && prop[1] ? prop[1] : prop) + 'Attribute'
-        if (_.Obj(name)) each(name, function (id, val) { el[isProp](dataAttr + id, val) })
+        if (_.Obj(name)) goEach(name, function (id, val) { el[isProp](dataAttr + id, val) })
         else el[isProp](dataAttr + name, funcArg(el, value, i, el.getAttribute(dataAttr + name)))
       })
     }
@@ -439,7 +430,7 @@
   // //////////////////////// //
   // *  EVENTS GROUP  * //
   // /////////////////////// //
-  each({ on: 'addEventListener', off: 'removeEventListener' }, function (type, position) {
+  goEach({ on: addEventListener, off: removeEventListener }, function (type, position) {
     Q.fn[type] = function (event, callback) {
       return this.each(function (i, el) {
         _.Str(event) && event.split(/\s+/).forEach(function (e) {
@@ -468,7 +459,7 @@
       return this
     }
   }
-  each({ 'show': 'block', 'hide': 'none', 'toggle': [] }, function (type, prop) {
+  goEach({ 'show': 'block', 'hide': 'none', 'toggle': [] }, function (type, prop) {
     Q.fn[type] = function (duration) {
       this.css({ 'visibility': 'initial', 'opacity': 1 })
       this.each(function (i, el) {
@@ -480,18 +471,18 @@
   // //////////////////////// //
   // *  DIMENSIONS GROUP  * //
   // /////////////////////// //
-  each(['Width', 'Height'], function (id, prop) {
+  goEach(['Width', 'Height'], function (id, prop) {
     var property = prop.toLowerCase()
     Q.fn[property] = function (value) {
       var el = this[0]
       if (_.Und(value)) {
         return isWindow(el) ? el['inner' + prop]
-          : _.Doc(el) ? el.documentElement['scroll' + prop]
-            : el[getBCR]()[property]
+          : _.Doc(el) ? el[documentElement]['scroll' + prop]
+            : el[getBoundingClientRect]()[property]
       }
       return goStyle(this, property, value)
     }
-    each(['outer', 'inner'], function (i, name) {
+    goEach(['outer', 'inner'], function (i, name) {
       Q.fn[name + prop] = function (includeMargins) {
         var el = this[0]
         return el && el[(!i ? 'offset' : 'client') + prop] + (includeMargins && !i ? float(getStyle(el, 'margin' + (id ? 'Top' : 'Left'))) + float(getStyle(el, 'margin' + (id ? 'Bottom' : 'Right'))) : 0)
@@ -503,9 +494,9 @@
   Q.isWindow = isWindow
   Q.type = isType
   Q.extend = extend
-  Q.each = each
+  Q.each = goEach
   Q.isPlainObject = isPlainObject
   Q.parseHTML = parseHTML
-  W[S] = W['Q'] = Q
-  _.Und(W.$) && (W.$ = Q)
-})(window, document, 'weeKit')
+  window[S] = Q
+  _.Und(window.$) && (window.$ = Q)
+})('weeKit', window, document, 'documentElement', 'nextElementSibling', 'previousElementSibling', 'parentElement', 'children', 'addEventListener', 'removeEventListener', 'querySelector', 'querySelectorAll', 'getBoundingClientRect', 'innerHTML', 'textContent', 'appendChild')
